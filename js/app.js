9a53651cef0191499bbd7fa618c041a60d4d90da
Djo917 <trstenjak.jonathan@gmail.com>
2021-10-18 11:22:01 +0200

    retrait des doublons dans la liste des ingrédients et des ustensiles

--- js/app.js
+++ js/app.js
@@ -1,19 +1,87 @@
-// import { Ajax } from './ajax.class.js';
-class Ajax {
-    constructor(url) {
-        this.url = url;
+import { Ajax } from './ajax.class.js';
+
+
+class IndexPage {
+    constructor(ajax) {
+        this.ajax = ajax;
     }
-    
-    async fetchData() {
-        const response = await fetch(this.url);
-        return await response.json();
+
+    run() {
+        this.getRecipes();
+        this.getDevices();
+        this.getUstensils();
+    }
+
+    getRecipes() {
+        const datas = this.ajax.fetchData();
+        const selectRecipes = document.querySelector(".filteroptions--recipes");
+        
+        datas.then(data => {
+            let wholeComponents = []
+            
+            data.recipes.forEach(recettes => {
+                recettes.ingredients.forEach(ingredient => {
+                    wholeComponents.push(ingredient.ingredient);  
+                })
+            })
+
+            let sortedComponents = [... new Set(wholeComponents)];
+
+            sortedComponents.forEach(compo => {
+                this.displayComponents(selectRecipes, compo);
+            })
+        })
+    }
+
+    getDevices() {
+        const datas = this.ajax.fetchData();
+        const selectDevices = document.querySelector(".filteroptions--device ");
+
+        datas.then(data => {
+            let wholeDevices = [];
+
+            data.recipes.forEach(devices => {
+                wholeDevices.push(devices.appliance);  
+            })
+
+            let sortedDevices = [...new Set(wholeDevices)];
+            
+            sortedDevices.forEach(compo => {
+                this.displayComponents(selectDevices, compo);
+            })
+        })
+    }
+
+    getUstensils() {
+        const datas = this.ajax.fetchData();
+        const selectUstensils = document.querySelector(".filteroptions--ustensils ");
+
+        datas.then(data => {
+            let wholeUstensils = [];
+
+            data.recipes.forEach(ustensils => {
+                ustensils.ustensils.forEach(ust => {
+                    wholeUstensils.push(ust);
+                })
+                
+            })
+
+            let sortedUstensils = [...new Set(wholeUstensils)];
+            
+            sortedUstensils.forEach(compo => {
+                this.displayComponents(selectUstensils, compo);
+            })
+        })
+    }
+
+    displayComponents(type, compo) {
+        const options = document.createElement("option");
+
+        type.appendChild(options);
+        options.value = `${compo}`;
+        options.innerText = `${compo}`;
     }
 }
 
-new Ajax('./data/recipes.js');
-
-const datas = this.ajax.fetchData();
-
-datas.then(data => {
-    console.log(data)
-})
+const indexPage = new IndexPage(new Ajax('./data/recipes.json'));
+indexPage.run();
